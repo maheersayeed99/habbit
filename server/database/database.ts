c1b1ad9ab5f138a6df28769b2efff53e53a50533
Maheer Sayeed <maheersayeed@gmail.com>
2023-05-28 00:03:39 -0400

    connection string used

--- server/database/database.ts
+++ server/database/database.ts
@@ -14,11 +14,12 @@ const {Pool} = require('pg');
 // });
 
 const pool = new Pool({
-    host: 'habbit.postgres.database.azure.com',
-    user: 'habbit',
-    password: 'Baba313104',
-    database: 'habbit',
-    port: 5432,
+    // host: 'habbit.postgres.database.azure.com',
+    // user: 'habbit',
+    // password: 'Baba313104',
+    // database: 'habbit',
+    // port: 5432,
+    connectionstring: 'host=habbit.postgres.database.azure.com port=5432 dbname=habbit user=habbit password=Baba313104 sslmode=require',
     max : 10,
     connectionTimeoutMillis : 0,
     idleTimeoutMillis : 0
