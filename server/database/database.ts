7353ffa2f08f304fa7602e430c3bf02c7d840509
Maheer Sayeed <maheersayeed@gmail.com>
2023-05-27 22:40:27 -0400

    changed database credentials

--- server/database/database.ts
+++ server/database/database.ts
@@ -3,11 +3,21 @@ import express from "express"
 const {Pool} = require('pg');
  
 
+// const pool = new Pool({
+//     host : 'localhost',
+//     user: 'postgres',
+//     port: 5432,
+//     database: 'habbit_db',
+//     max : 10,
+//     connectionTimeoutMillis : 0,
+//     idleTimeoutMillis : 0
+// });
+
 const pool = new Pool({
-    host : 'localhost',
-    user: 'postgres',
-    port: 5432,
-    database: 'habbit_db',
+    host: 'habbit.postgres.database.azure.com',
+    user: 'habbit',
+    password: 'Baba313104',
+    database: 'habbit',
     max : 10,
     connectionTimeoutMillis : 0,
     idleTimeoutMillis : 0
