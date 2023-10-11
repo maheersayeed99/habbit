bfe9b0bd23b18b53d672a06da5c62bc9cf1956cf
Maheer Sayeed <maheersayeed@gmail.com>
2023-10-10 21:41:23 -0400

    react components working correctly

--- client/src/components/row.tsx
+++ client/src/components/row.tsx
@@ -1,23 +1,44 @@
 import React from "react";
 import "../stylesheets/table.css"
 
-const Row = (progress_class:string, percentage:number, activity: string, brightness:number, streak:number, max_streak:number, max_brightness:number) => {
+const Row = (props) => {
+
+
+    var progress_class = "";        
+        
+    if (props.data.completed == false) {
+        progress_class = "red-progress";
+    }
+    else if(props.data.completed_today == false) {
+        progress_class = "yellow-progress";
+    }
+    else{
+        progress_class = "green-progress";
+    }
+
+    var percentage = 100*(props.data.progress/props.data.frequency);
+    const floor = 0.80;
+    const ceiling = 1.5;
+    const max_streak = 30;
+    var streak_brightness = floor + ((Math.min(props.data.streak,max_streak)/max_streak)* (ceiling-floor));
+    var max_brightness = floor + ((Math.min(props.data.max_streak,max_streak)/max_streak)* (ceiling-floor));
+    var activity = props.data.activity[0].toUpperCase() + props.data.activity.slice(1)
     
     return (
         
         <tr>
-            
+
             <td className={`activity-cell ${progress_class}`} >
                 <div className={`activity-progress ${progress_class}`} style={ {width: `${percentage}%`}}></div>
                 <span className="activity-text">{activity}</span>
             </td>
 
-            <td className="streak-cell" style={{filter: `brightness(${brightness});`}}>
-                <div className="streak-text">{streak}</div>
+            <td className="streak-cell" style={{filter: `brightness(${streak_brightness})`}}>
+                <div className="streak-text">{props.data.streak}</div>
             </td>
             
-            <td className="max-cell" style={{filter: `brightness(${max_brightness});`}}>
-                <div className="streak-text">{max_streak}</div>
+            <td className="max-cell" style={{filter: `brightness(${max_brightness})`}}>
+                <div className="streak-text">{props.data.max_streak}</div>
             </td>
 
         </tr>
--- client/src/components/table.tsx
+++ client/src/components/table.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../stylesheets/table.css"
-// import { Row } from "./row.tsx";
+import { Row } from "./row.tsx";
 import { useState, useEffect} from "react";
 import axios from "axios"
 
@@ -9,17 +9,13 @@ const Table : React.FC = () => {
     const [table_data, get_data] = useState([])
 
     const updateTable = async () => {
-        const table_response = await axios.get("http://localhost:8080/api/main", {headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-          }})
-        console.log(await table_response);
-        // const json_response = await table_response.json()
-        // console.log(json_response)
-        // get_data(json_response)
+        const table_response = await axios.get("/api/main")
+        console.log(await table_response.data);
+        get_data(table_response.data)
     }
 
-    updateTable();
+    useEffect(()=>{updateTable()},[])
+
     return (
         <div className="table-container">
             <table>
@@ -29,10 +25,17 @@ const Table : React.FC = () => {
                     <th colSpan={2}>Streak / Max</th>
                 </tr>
                 </thead>
+                <tbody>
+
+                {table_data.map((item, index) => (
+                    <Row data={item}/>
+                ))}
+
+                </tbody>
                 
             </table>
+            
             <span>
-            {table_data}
             </span>
         </div>
         
