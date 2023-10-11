0f7be83dcf4748b27e3beeb523805b401194112e
Maheer Sayeed <maheersayeed@gmail.com>
2023-10-11 00:35:22 -0400

    ready for merge

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import express from "express"
 import {router} from "./routes/routes" 
 import { Express } from "express";
 import bodyParser from "body-parser";
-import {render_html, get_data} from "./render"
 import cors from "cors"
 
 
@@ -12,6 +11,17 @@ require('dotenv').config()
 // const schedule = require('node-schedule');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log("app running");
+
+app.use("/api", router);
+
 // const update_hour : number = 9;
 // const update_minute : number = 30;
 
@@ -45,15 +55,7 @@ const app = express();
 //     }
 // }
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-const port = process.env.PORT || 8080;
-app.listen(port);
-console.log("app running");
-
-app.use("/api", router);
 
 // These would work if app was running all the time on a better plan
 // const job = schedule.scheduleJob(`${update_minute} ${update_hour} * * *`, () => {
@@ -67,22 +69,6 @@ app.use("/api", router);
 
 // }, 60000)
 
- 
-app.get("/", (req, res) => {
-
-    // var date = new Date();
-    // var hour = date.getHours();
-    // var minute = date.getMinutes();
-    // console.log("HOUR" + hour);
-    //res.json(hour + ":" + minute);
-    
-    res.setHeader("Content-Type", "text/html")
-    get_data()
-    .then((table_data) => {
-        res.send(render_html(table_data));
-    })
-
-} );
 
 
 export { router }
